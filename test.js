e5a888abf10e7a5060ec0305fdf4a1a6db179100
Aman Chaurasiya <chaurasiyaaman741@gmail.com>
2025-11-06 21:22:16 +0530

    feat: add CI pipeline

---
+++ test.js
@@ -0,0 +1,9 @@
+console.log('====================================');
+console.log("Starting test....");
+console.log('====================================');
+
+setTimeout(() => {
+    console.log("waiting for test complete......");
+}, 3000);
+
+console.log("Test complete.....");
